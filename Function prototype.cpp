ac734d024079f827d05eca1495a99d60f1dfead1
Tarun Sheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-10-23 21:50:13 +0530

    Add files via upload

---
+++ Function prototype.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+ 
+ int sum(int, int);
+ void g();
+ 
+ int main() { 
+ 	int num1, num2;
+ 	cout<<"Enter first number ="<<endl;
+ 	cin>>num1;
+ 	cout<<"Enter second number ="<<endl;
+ 	cin>>num2;
+ 	//cout<<"The sum is = "<<num1+num2;
+ 	cout<<"The sum is = "<<sum(num1, num2);
+ 	g();
+ 	return 0;
+ }
+ int sum(int a, int b) {
+ 	int c=a+b;
+ 	return c;
+ };
+ void g(){
+ 	cout<<"\nHello, good Morning!";	
+ };
+
